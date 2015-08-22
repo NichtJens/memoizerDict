98c96f8feb49fe2cf91611a567edec6f22abda61
Auf keinen Fall Jens <NichtJens@users.noreply.github.com>
2015-08-22 15:47:27 +0200

    Slightly simplified the code, corrected the docstrings

--- memodict.py
+++ memodict.py
@@ -14,17 +14,15 @@ class memoized(dict):
         self.func = func
 
     def __call__(self, *args):
-        """Build a key, return dict item for that key"""
-        self.args = args
+        """Return dict item for args as key"""
         return self[args]
 
     def __missing__(self, key):
         """Is called for missing keys, calculates and stores them"""
-        self[key] = self.func(*self.args)
+        self[key] = self.func(*key)
         return self[key]
 
     @property
     def cache(self):
         return [(x, y) for x, y in sorted(self.items())]
-
 
